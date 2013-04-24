051638968b849a8a56695516b411e6752bdba655
Chad Felten <chadfelten@gmail.com>
2013-04-24 16:41:56 +0000

    4/24 update

--- challenge4.py
+++ challenge4.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# NOT COMPLETE
-
-
 import os
 import pyrax
 import re
@@ -15,10 +12,12 @@ dns = pyrax.cloud_dns
 ip_regex = r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
 fqdn_regex = r'(?=^.{1,254}$)(^(?:(?!\d+\.)[a-zA-Z0-9_\-]{1,63}\.?)+(?:[a-zA-Z]{2,})$)'
 
-dom = dns.create(name="my.example.com", emailAddress="admin@example.com", ttl=600)
+domain_name = 'derekfelten.com'
+dom = dns.find(name=domain_name)
+print('Adding record for',dom)
 
 while True:
-    fqdn = raw_input("enter fqdn: ")
+    fqdn = 'derekfelten.com'
     fqdn_search = re.search(fqdn_regex, fqdn)
     if fqdn_search:
         break
@@ -26,7 +25,7 @@ while True:
         print "FQDN is not formatted correctly."
     
 while True:
-    ip = raw_input("enter ip: ")
+    ip = '97.74.42.79'
     ip_search = re.search(ip_regex, ip)
     if ip_search:
         break
--- challenge9.py
+++ challenge9.py
@@ -10,36 +10,78 @@ creds_file = os.path.expanduser("~/.cred")
 pyrax.set_credential_file(creds_file)
 cs_ord = pyrax.connect_to_cloudservers(region="ORD")
 cs = pyrax.cloudservers
+dns = pyrax.cloud_dns
+
 
 #Ubuntu 12.04 LTS
-#image = '5cebb13a-f783-4f8c-8058-c4182c724ccd'
+image = '5cebb13a-f783-4f8c-8058-c4182c724ccd'
 #512MB 20GB disk
 flavor = 2 
 #name
-fqdn = 'www.example.com'
-
-#server = cs.servers.create(srv, image, flavor)
-
-
-#use argparse -images to list all the images in a dictionary format
+fqdn = 'www.derekfelten.com'
 
 parser = argparse.ArgumentParser()
-#list images
-parser.add_argument("-li", "--listimages", action="store_true",help="list cloud server images")
-parser.add_argument("-i", "--image", action="store_true",help="image to use for cloud server")
-#list flavors
-#parser.add_argument("-lf", "--listflavors", action="store_true",help="list cloud server images")
-#enter name
-#parser.add_argument("-f", "--fqdn", action="store_true",help="a")
-args = parser.parse_args()
-if args.listimages:
-    imgs = cs.images.list()
-    for img in imgs:
-        print img.name
 
-if args.image:
-   image = [img for img in cs.images.list()
-        if "" in img.name][0]
+parser.add_argument("-n", "--name", help="name to use for  cloud server")
+parser.add_argument("-i", "--image", help="image to use for cloud server")
+parser.add_argument("-f", "--flavor",  help="flavor to use for cloud server")
+args = parser.parse_args()
+
+server = cs.servers.create(args.name, args.image, args.flavor)
+networks = server.networks
+
+#server build steps
+while not networks:
+    server = cs.servers.get(server.id)
+    networks =  server.networks
+ip_regex = r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
+ip_search = re.search(ip_regex, networks['public'][0])
+if ip_search:
+    ip = networks['public'][0]
+else:
+    ip = networks['public'][1]
+
+#DNS record creation
+domain_name = 'derekfelten.com'
+dom = dns.find(name=domain_name)
+
+rec = [{
+        "type": "A",
+        "name": fqdn,
+        "data": ip,
+        }]
+
+print dom.add_records(rec)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     
     
